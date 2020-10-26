6a7ed33e10a61d7ebe1c300f3da11dff757cedf6
Jonathan Rodrigues Santos <jrsantos@everis.com>
2020-10-26 16:13:35 -0300

    first commit

--- src/repository/Repository.java
+++ src/repository/Repository.java
@@ -22,6 +22,18 @@ public class Repository {
 		produto.Imposto = 4.0;
 
 		fipe.add(produto);
+		
+		produto = new Produto();
+
+		produto.Modelo = "Uno";
+		produto.Ano = 2005;
+		produto.Categoria = "Hatc";
+		produto.Valor = 10000;
+		produto.Fabricante = "FIAT";
+		produto.QtdPortas = 2;
+		produto.Imposto = 4.0;
+
+		fipe.add(produto);
 
 		return fipe.get(indice);
 
